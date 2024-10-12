f68a4a141278660cecc7abf87c6dc13a28b3a586
Nick <nikita.khodzhaiev@gmail.com>
2024-10-12 11:04:54 +0300

    Adding temp project

---
+++ Projects (temp)/Example/Example/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Nikita Khodzhaiev on 08.05.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
